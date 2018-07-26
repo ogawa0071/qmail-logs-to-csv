d336e37a3f4f0e6df0d6f66c4672ab34c74e2218
Futa Ogawa <ogawa0071@gmail.com>
2018-07-27 03:28:33 +0900

    Simplify codeing.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import * as fs from 'fs'
-import * as path from 'path'
-import * as process from 'process'
-import * as util from 'util'
+import * as readline from 'readline'
 
 import Convert from './Convert'
-
-const fsReadFile = util.promisify(fs.readFile)
-const fsReaddir = util.promisify(fs.readdir)
 
 class QmailLogsToCsv extends Command {
   static description = 'qmail logs file convert to csv'
@@ -20,59 +15,32 @@ class QmailLogsToCsv extends Command {
     // name: flags.string({char: 'n', description: 'name to print'}),
     // flag with no value (-f, --force)
     // force: flags.boolean({char: 'f'}),
-    directory: flags.boolean({char: 'd'}),
   }
 
   static args = [{
     name: 'path',
     required: true,
-    description: 'file or directory path'
+    description: 'file path'
   }]
 
   async run() {
-    const {args, flags} = this.parse(QmailLogsToCsv)
-
+    const {args} = this.parse(QmailLogsToCsv)
     const convert = new Convert()
 
-    // Read file or directory
-    const logs = await this.getLogs(args, flags).catch(err => {
-      throw err
+    this.log('timestamp,email')
+
+    const fileStream = fs.createReadStream(args.path)
+    const rl = readline.createInterface(fileStream)
+
+    rl.on('line', log => {
+      const csvLine = convert.logLine(log)
+      if (csvLine) {
+        this.log(csvLine)
+      }
     })
-
-    // Convert logs to csv
-    const result = await convert.logs(logs).catch(err => {
-      throw err
+    rl.on('close', () => {
+      this.log('Complete!')
     })
-
-    // output console
-    this.log(result)
-  }
-
-  async getLogs(args: any, flags: any) {
-    if (flags.directory) {
-      const files = await fsReaddir(args.path).catch(() => {
-        throw new Error(`Can't read directory: ${args.path}`)
-      })
-
-      const filesBuffer = await Promise.all(files.map(async file => {
-        const filePath = path.join(process.cwd(), args.path, file)
-        const fileBuffer = await fsReadFile(filePath).catch(() => {
-          throw new Error(`Can't read file: ${filePath}`)
-        })
-        return fileBuffer
-      })).catch(err => {
-        throw err
-      })
-
-      const concatBuffer = Buffer.concat(filesBuffer)
-      return concatBuffer
-    }
-
-    const filePath = path.join(process.cwd(), args.path)
-    const fileBuffer = await fsReadFile(filePath).catch(() => {
-      throw new Error(`Can't read file: ${filePath}`)
-    })
-    return fileBuffer
   }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -5,15 +5,8 @@ import cmd = require('../src')
 describe('qmail-logs-to-csv', () => {
   test
   .stdout()
-  .do(() => cmd.run(['./test/test.log']))
-  .it('runs qmail-logs-to-csv ./test/test.log', ctx => {
-    expect(ctx.stdout).to.contain('timestamp,email\n2018-01-01T00:00:00.000Z,ogawa-futa@dmm.com\n')
-  })
-
-  test
-  .stdout()
-  .do(() => cmd.run(['-d', './test/directory/']))
-  .it('runs qmail-logs-to-csv -d ./test/directory', ctx => {
+  .do(() => cmd.run(['/Users/futa/GitHub/qmail-logs-to-csv/test/test.log']))
+  .it('runs qmail-logs-to-csv ~/GitHub/qmail-logs-to-csv/test/test.log', ctx => {
     expect(ctx.stdout).to.contain('timestamp,email\n2018-01-01T00:00:00.000Z,ogawa-futa@dmm.com\n')
   })
 })
